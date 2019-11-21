b80031a2a5867a4b362b924edd7f8b9567b30fa9
Ben Johnson <ben@learncodebygaming.com>
2019-11-21 13:47:16 -0600

    Image bot code and action scripts

---
+++ image_bot/controller.py
@@ -0,0 +1,171 @@
+from playback import initializePyAutoGUI, countdownTimer, playActions
+from time import sleep
+import pyautogui
+import os
+
+
+def main():
+    
+    initializePyAutoGUI()
+    countdownTimer()
+
+    LOOP_REPITITIONS = 3
+    for i in range(0, LOOP_REPITITIONS):
+
+        if i > 0:
+            # Wait for station to load
+            sleep(10.00)
+
+        # Start docked at Loki Station, get to our universal starting position
+        # get_starting_pos() should return an integer that corresponds with which
+        # recorded action script should be played
+        starting_pos = get_starting_pos("loki")
+        try:
+            action_filename = "lokistation_start_{}.json".format(starting_pos)
+            playActions(action_filename)
+        except FileNotFoundError as e:
+            if not starting_pos:
+                print("Starting position not recognized in Loki")
+            else:
+                print("No recorded actions for position {}".format(starting_pos))
+            exit(0)
+        else:
+            print("Played starting actions {}".format(action_filename))
+        sleep(2.00)
+
+        confirmPosition("lokistation_starting_pos")
+
+        # Playback those four steps to go to the trader, do the buying/selling,
+        # go back to the ship, fly to Earth Station
+        playActions("lokistation_goto_trader.json")
+        sleep(2.00)
+        playActions("lokistation_do_trading.json")
+        sleep(2.00)
+        confirmPosition("lokistation_done_trading")
+        playActions("lokistation_goto_ship.json")
+        sleep(10.00)  # allow time for loading screen when exiting station
+        playActions("lokistation_goto_earthstation.json")
+
+        # Wait for station to load
+        sleep(10.00)
+
+        # Now docked at Earth Station, get to that starting position
+        # get_starting_pos() should return an integer that corresponds with which
+        # recorded action script should be played
+        starting_pos = get_starting_pos("earth")
+        try:
+            action_filename = "earthstation_start_{}.json".format(starting_pos)
+            playActions(action_filename)
+        except FileNotFoundError as e:
+            if not starting_pos:
+                print("Starting position not recognized in Earth")
+            else:
+                print("No recorded actions for position {}".format(starting_pos))
+            exit(0)
+        else:
+            print("Played starting actions {}".format(action_filename))
+        sleep(2.00)
+
+        # Playback four steps to go to the trader, do the buying/selling,
+        # go back to the ship, fly to Loki Station
+        playActions("earthstation_goto_trader.json")
+        sleep(2.00)
+        playActions("earthstation_do_trading.json")
+        sleep(2.00)
+        playActions("earthstation_goto_ship.json")
+        sleep(10.00)  # allow time for loading screen when exiting station
+        playActions("earthstation_goto_lokistation.json")
+
+        # Completed loop
+        print("Completed loop")
+
+    print("Done")
+
+
+def get_starting_pos(station):
+
+    if station == 'loki':
+        images_to_check = [
+            'lokistation_start_1.png',
+            'lokistation_start_2.png',
+            'lokistation_start_3.png',
+            'lokistation_start_4.png',
+            'lokistation_start_5.png',
+            'lokistation_start_6.png',
+            'lokistation_start_7.png',
+            'lokistation_start_8.png',
+            'lokistation_start_9.png',
+            'lokistation_start_10.png',
+            'lokistation_start_11.png',
+            'lokistation_start_12.png',
+            'lokistation_start_13.png',
+            'lokistation_start_14.png',
+            'lokistation_start_15.png',
+            'lokistation_start_16.png',
+            'lokistation_start_17.png',
+            'lokistation_start_18.png',
+            'lokistation_start_19.png',
+        ]
+    elif station == 'earth':
+        images_to_check = [
+            'earthstation_start_1.png',
+            'earthstation_start_2.png',
+            'earthstation_start_3.png',
+            'earthstation_start_4.png',
+            'earthstation_start_5.png',
+            'earthstation_start_6.png',
+            'earthstation_start_7.png',
+            'earthstation_start_8.png',
+            'earthstation_start_9.png',
+            'earthstation_start_10.png',
+            'earthstation_start_11.png',
+            'earthstation_start_12.png',
+            'earthstation_start_13.png',
+            'earthstation_start_14.png',
+            'earthstation_start_15.png',
+        ]
+    
+    # loop over images until one is found, then return the index of the found
+    # image
+    for index, image_filename in enumerate(images_to_check):
+        script_dir = os.path.dirname(__file__)
+        needle_path = os.path.join(
+            script_dir, 
+            'needles', 
+            image_filename
+        )
+        image_pos = pyautogui.locateOnScreen(needle_path)
+        if image_pos:
+            return index + 1
+
+    return None
+
+
+def confirmPosition(position_name):
+
+    if position_name == "lokistation_starting_pos":
+        x, y = (898, 153)
+        rgb = (172, 178, 41)
+    elif position_name == "lokistation_done_trading":
+        x, y = (450, 296)
+        rgb = (79, 78, 13)
+    else:
+        raise Exception("Position to confirm not recognized")
+
+    pixel_matches = pyautogui.pixelMatchesColor(x, y, rgb, tolerance=10)
+    if not pixel_matches:
+        debug_str = "Pos: {} RGB expected: {} RGB found: {}".format(
+            (x, y),
+            rgb,
+            pyautogui.pixel(x, y)
+        )
+        raise Exception("Detected off course for {}. Debug: {}".format(
+            position_name, 
+            debug_str
+        ))
+
+    print("On track for {}".format(position_name))
+
+
+if __name__ == "__main__":
+    main()
---
+++ image_bot/playback.py
@@ -0,0 +1,104 @@
+import pyautogui
+from time import sleep, time
+import os
+import json
+
+
+def initializePyAutoGUI():
+    # Initialized PyAutoGUI
+    # https://pyautogui.readthedocs.io/en/latest/introduction.html
+    # When fail-safe mode is True, moving the mouse to the upper-left corner will abort your program.
+    pyautogui.FAILSAFE = True
+
+
+def countdownTimer():
+    # Countdown timer
+    print("Starting", end="")
+    for i in range(0, 6):
+        print(".", end="")
+        sleep(1)
+    print("Go")
+
+
+def playActions(filename):
+    # Read the file
+    script_dir = os.path.dirname(__file__)
+    filepath = os.path.join(
+        script_dir, 
+        'recordings', 
+        filename
+    )
+    with open(filepath, 'r') as jsonfile:
+        # parse the json
+        data = json.load(jsonfile)
+        
+        # loop over each action
+        # Because we are not waiting any time before executing the first action, any delay before the initial
+        # action is recorded will not be reflected in the playback.
+        for index, action in enumerate(data):
+            action_start_time = time()
+
+            # look for escape input to exit
+            if action['button'] == 'Key.esc':
+                break
+
+            # perform the action
+            if action['type'] == 'keyDown':
+                key = convertKey(action['button'])
+                pyautogui.keyDown(key)
+                print("keyDown on {}".format(key))
+            elif action['type'] == 'keyUp':
+                key = convertKey(action['button'])
+                pyautogui.keyUp(key)
+                print("keyUp on {}".format(key))
+            elif action['type'] == 'click':
+                pyautogui.click(action['pos'][0], action['pos'][1], duration=0.25)
+                print("click on {}".format(action['pos']))
+
+            # then sleep until next action should occur
+            try:
+                next_action = data[index + 1]
+            except IndexError:
+                # this was the last action in the list
+                break
+            elapsed_time = next_action['time'] - action['time']
+
+            # if elapsed_time is negative, that means our actions are not ordered correctly. throw an error
+            if elapsed_time < 0:
+                raise Exception('Unexpected action ordering.')
+
+            # adjust elapsed_time to account for our code taking time to run
+            elapsed_time -= (time() - action_start_time)
+            if elapsed_time < 0:
+                elapsed_time = 0
+            print('sleeping for {}'.format(elapsed_time))
+            sleep(elapsed_time)
+
+
+# convert pynput button keys into pyautogui keys
+# https://pynput.readthedocs.io/en/latest/_modules/pynput/keyboard/_base.html#Key
+# https://pyautogui.readthedocs.io/en/latest/keyboard.html
+def convertKey(button):
+    PYNPUT_SPECIAL_CASE_MAP = {
+        'alt_l': 'altleft',
+        'alt_r': 'altright',
+        'alt_gr': 'altright',
+        'caps_lock': 'capslock',
+        'ctrl_l': 'ctrlleft',
+        'ctrl_r': 'ctrlright',
+        'page_down': 'pagedown',
+        'page_up': 'pageup',
+        'shift_l': 'shiftleft',
+        'shift_r': 'shiftright',
+        'num_lock': 'numlock',
+        'print_screen': 'printscreen',
+        'scroll_lock': 'scrolllock',
+    }
+
+    # example: 'Key.F9' should return 'F9', 'w' should return as 'w'
+    cleaned_key = button.replace('Key.', '')
+
+    if cleaned_key in PYNPUT_SPECIAL_CASE_MAP:
+        return PYNPUT_SPECIAL_CASE_MAP[cleaned_key]
+
+    return cleaned_key
